a2f3d792373749906a06dc90cc25e6d329ae0b3a
Remi Mikalsen <remimikalsen@gmail.com>
2025-02-11 18:59:03 +0100

    Wrongly named file + improved chunking

--- kasumi_frontend/src/hooks.server.js
+++ kasumi_frontend/src/hooks.server.js
@@ -6,8 +6,12 @@ export const handle = async ({ event, resolve }) => {
     const plausibleScript = env.PUBLIC_ANALYTICS_SCRIPT || '';
   
     const response = await resolve(event, {
-      transformPageChunk: ({ html }) =>
-        html.replace('%analytics_script%', plausibleScript)
+      transformPageChunk: ({ html, done }) => {
+        if (done) {
+          return html.replace('%analytics_script%', plausibleScript);
+        }
+        return html;
+      }
     });
   
     return response;
