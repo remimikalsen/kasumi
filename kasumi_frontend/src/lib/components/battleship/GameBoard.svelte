c3eae87a86ca4074050c93a0b3b69a28bf2495a2
Remi Mikalsen <remimikalsen@gmail.com>
2025-04-05 21:41:38 +0200

    Change gap

--- kasumi_frontend/src/lib/components/battleship/GameBoard.svelte
+++ kasumi_frontend/src/lib/components/battleship/GameBoard.svelte
@@ -702,7 +702,7 @@
     .game-board {
         display: flex;
         flex-direction: column;
-        gap: 2rem;
+        gap: 0.5rem;
         width: 100%;
         max-width: 800px;
         margin: 0 auto;
