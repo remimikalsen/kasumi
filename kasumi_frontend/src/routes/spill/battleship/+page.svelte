a46f5417ee5eb24f9cd9f5addf9c253cdc09df4f
Remi Mikalsen <remimikalsen@gmail.com>
2025-04-06 13:12:33 +0200

    Moving all game logics into lib

--- kasumi_frontend/src/routes/spill/battleship/+page.svelte
+++ kasumi_frontend/src/routes/spill/battleship/+page.svelte
@@ -3,15 +3,6 @@
     import { env } from '$env/dynamic/public';
     import { getLocalizedText, loadTexts, pageHeader, pageSubHeader, documentTitle } from '$lib/stores/translatedTexts.js';
     import BattleshipGame from '$lib/components/battleship/BattleshipGame.svelte';
-    import GameSetup from '$lib/components/battleship/GameSetup.svelte';
-    import { battleshipConfig } from '$lib/config/battleshipConfig.js';
-    import type { GameState } from '$lib/services/battleshipServices';
-    import { battleshipApi } from '$lib/services/battleshipServices';
-
-    let gameStarted = false;
-    let playerInitials = "";
-    let gameId: string | null = null;
-    let gameState: GameState | null = null;
     
     // Load language texts
     let pageTexts = 'battleship';
@@ -36,45 +27,6 @@
         pageSubHeader.set(getLocalizedText(pageTexts, 'headerSubTitle'));
         documentTitle.set(getLocalizedText(pageTexts, 'headerTitle') + ' - ' + getLocalizedText(commonTexts, 'documentTitle' + alt));
     }
-    
-    async function handleGameStart(event: CustomEvent<{username: string, gameId: string}>) {
-        playerInitials = event.detail.username;
-        gameId = event.detail.gameId;
-
-        const gameStateResponse = await battleshipApi.getGameState(gameId, playerInitials);
-        gameState = gameStateResponse.gameState;
-
-        gameStarted = true;
-    }
-
-    function handleGameDone() {
-        gameStarted = false;
-    }
 </script>
 
-    {#if !gameStarted}
-        <GameSetup on:gameStart={handleGameStart} />
-    {:else}
-        <BattleshipGame 
-            username={playerInitials} 
-            gameState={gameState}
-            on:done={handleGameDone}
-        />
-    {/if}
-    
-    {#if battleshipConfig.debugOpponentBoard}
-        <div class="env-info">
-            <p>Debugging opponent's board</p>
-        </div>
-    {/if}
-
-<style>
-
-    .env-info {
-        margin-top: 2rem;
-        font-size: 0.8rem;
-        color: #7f8c8d;
-    }
-    
-
-</style> 
+<BattleshipGame />
